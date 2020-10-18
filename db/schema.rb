92ed7e9a30a250251d4642867235c08ed1a9cefa
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2020-10-17 22:22:26 -0300

    (migration) add name field to attendee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_234119) do
+ActiveRecord::Schema.define(version: 2020_10_18_003854) do
 
   create_table "attendees", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_234119) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_attendees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_attendees_on_reset_password_token", unique: true
   end
