2cf67e835f6d0a54c8e89a8318034a444a8f0d2f
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2020-10-17 22:22:56 -0300

    (attendee) permitted name text_field for sign_up

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,4 +3,6 @@ class Attendee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
 end
