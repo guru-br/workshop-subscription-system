c94027c76acf85163ffce9afb16b1ae6c436d975
Joao Almeida <joaorsalmeida@gmail.com>
2020-10-28 20:32:22 -0300

    Create scenarios for enrollment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,12 +33,12 @@ end
 RSpec.configure do |config|
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
